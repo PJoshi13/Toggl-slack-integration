11daeb301245d0710ff64d83621461fbceadc903
Prashant <pjosh@seas.upenn.edu>
2015-03-23 19:09:08 -0400

    tests getTasks

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 token = "1e8e441f4cf317c4efc264b84aee1375"
 headers = {'content-type': 'application/json'}
 
-@app.route('/toggl start', methods=['GET', 'POST'])
+@app.route('/toogl start' methods=['GET', 'POST'])
 def get_tasks():
 	url = 'https://www.toggl.com/api/v8/workspaces/476356/tasks'
 	headers = {'content-type': 'application/json'}
@@ -18,72 +18,29 @@ def get_tasks():
 	g = requests.GET(url, headers=headers, auth=HTTPBasicAuth(token, 'api_token'))
 	import pdb; pdb.set_trace()
 	return g.json()
-def send_data(key, params=None, data=None):
-    '''
-    Use the api to send data.
-    params: A dictionary that will be urlencoded
-    Returns a dictionary.
-    '''
-    headers = {"Content-Type": "application/json"}
-    key = token
 
-    # JSON Encode the data dict
-    data=simplejson.dumps(data)
-    with session(headers=headers) as r:
-        response = r.post(api(key), data=data)
-        content = response.content
-        if response.ok:
-            json = simplejson.loads(content)
-            return json["data"]
-        else:
-            exit("Please verify your login credentials...")
-
-@app.route('/toggl start', methods=['GET', 'POST'])
+@app.route('/toggl starts', methods=['GET', 'POST'])
 def start_task():
     url = 'https://www.toggl.com/api/v8/time_entries/start'
     headers = {'content-type': 'application/json'}
-
+    orig - request.form['text']
     start_time = datetime.datetime.utcnow()
     local_start_time = datetime.datetime.now()
 
     data = {
-        "start": start_time.isoformat(),
-        "stop": "null",
-        "description": orig}
+        "id":1573236,
+        "pid":123,
+        "wid":476356,
+        "billable": false,
+        "start": start_time,
+        "duration": -calendar.timegm(time.gmtime()),
+        "description":"Meeting with possible clients",
+        "tags":["billed"]
+        }
 
-    # If task Id was specified, add it to the data dict
-    if taskID:
-        data["task"] = {"id":taskID}
-
-    # Add to time_entry key
-    data = {"time_entry" : data }
-
-
-    send_data("time_entries", data=data)
-
-    p = requests.POST(url, headers=headers, auth=HTTPBasicAuth(token, 'api_token'))
+    r = requests.get(url, params=data)
     import pdb; pdb.set_trace()
     return p.json()
-def new_time_entry(description, taskID=False):
-                    
-    # Get the current time and store it. Then pause until the user
-    # says they are finished timing the task. Get the time they stopped
-    # the timer, subtract it from the start_time, and store the difference
-    # in seconds as the duration.
-    #start_time = datetime.datetime.now()
-    start_time = datetime.datetime.utcnow()
-    local_start_time = datetime.datetime.now()
-
-
-    send_data("time_entries", data=data)
-
-    print "Sending data..."
-
-    end_time = datetime.datetime.utcnow()
-    time_difference = (end_time - start_time).seconds
-
-
-    # Data passed to the request
 
 if __name__ == '__main__':
     app.run(host='104.131.72.152:5000')
